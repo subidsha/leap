46e031bdfdb9a4648133ee4f7afed85c1c3e4d79
subidsha <31504698+subidsha@users.noreply.github.com>
2017-09-23 21:51:31 +0530

    Update year.c

--- year.c
+++ year.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
-#include<string.h>
+void main()
 {
-char a=hello;
-printf("the word is",a);
-for(i=1;i<=5;i++)
-{
-printf("a");
-}
-}
+  int n;
+  printf("enter the number\n",n);
+  scanf("%d"n);
+  for(i=1;i<=n;i++)
+  {
+    printf("i");
+  }}
